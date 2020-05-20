c48908a6baf47fadd64a3c54e9447e81c7332460
Marcin <panekmarcin95@gmail.com>
2020-05-20 22:48:52 +0200

    first commit

---
+++ src/main/java/pl/wat/wcy/panek/saaggregator/SaaggregatorApplication.java
@@ -0,0 +1,13 @@
+package pl.wat.wcy.panek.saaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaaggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaaggregatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wat/wcy/panek/saaggregator/SaaggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wat.wcy.panek.saaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaaggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
