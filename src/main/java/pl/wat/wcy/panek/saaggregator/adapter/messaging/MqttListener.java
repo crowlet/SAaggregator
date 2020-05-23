5f1c54837e34f7fbf6137f4e5ec23859d40c77f6
Marcin <panekmarcin95@gmail.com>
2020-05-23 20:12:19 +0200

    dodatkowy log

--- src/main/java/pl/wat/wcy/panek/saaggregator/adapter/messaging/MqttListener.java
+++ src/main/java/pl/wat/wcy/panek/saaggregator/adapter/messaging/MqttListener.java
@@ -1,6 +1,7 @@
 package pl.wat.wcy.panek.saaggregator.adapter.messaging;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import pl.wat.wcy.panek.saaggregator.application.subject.SubjectAppService;
 import pl.wat.wcy.panek.saaggregator.domain.subject.SubjectDomainService;
@@ -9,11 +10,13 @@ import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MqttListener {
 
     private final SubjectAppService subjectService;
 
     public void event(Map<String, Object> message) {
+        log.info(message.toString());
         subjectService.update(message);
     }
 
