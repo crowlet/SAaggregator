173931c57beddda3e1fa69c9f51acded2cccae32
Marcin <panekmarcin95@gmail.com>
2020-05-23 17:17:03 +0200

    log łączenia z mqtt

--- src/main/java/pl/wat/wcy/panek/saaggregator/config/MqttConfig.java
+++ src/main/java/pl/wat/wcy/panek/saaggregator/config/MqttConfig.java
@@ -1,5 +1,6 @@
 package pl.wat.wcy.panek.saaggregator.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class MqttConfig {
 
     @Bean
@@ -20,6 +22,7 @@ public class MqttConfig {
     ) throws MqttException {
 
         var mqttClient = new MqttClient(mqttBrokerUrl, publisherIdProvider.id());
+        log.info("Connecting to: " + mqttBrokerUrl);
         mqttClient.connect(options);
         return mqttClient;
     }
